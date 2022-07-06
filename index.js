bd957ecd7be06326e1a7a2c8e7fc5c79f0cefdde
Danielle Moylan <dmmoylan@sau29.org>
2022-07-06 13:25:10 -0400

    added updateCount function-count currently undefined

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const cardSuits = ["♥", "♦", "♠", "♣"];
 const cardValues = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
 const deck = [];
-let player1Cards, player2Cards, currentCardCount
+let player1Cards, player2Cards, p1CardCount, p2CardCount;
 
 //Build deck of cards
 function newDeck() {
@@ -52,19 +52,35 @@ function startGame() {
     player1Cards = deck.slice(0, deck.length / 2);
     player2Cards = deck.slice((deck.length / 2 ));
     currentCard = 0;
+    p1CardCount = 26;
+    p2CardCount = 26;
 
      //testing purposes
     console.log(player1Cards);
     console.log(player2Cards);
-    }
+}
 
-function roundWinner(p1Card, p2Card) {
+// Decide who wins round, update cardCount for each player
+function roundWinner(p1Card, p2Card, p1CardCount, p2CardCount) {
     var p1Index = cardValues.indexOf(p1Card.value);
     var p2Index = cardValues.indexOf(p2Card.value);
 
-    if (p1Index > p2Index) return 'Player 1 wins this round.';
-    if (p2Index > p1Index) return 'Player 2 wins this round.';
-    return 'It is a tie. Flip again.'
+    if (p1Index > p2Index) {
+        updateCount(p1CardCount, p2CardCount);
+        p1CardCount++;
+        p2CardCount--;
+        return 'Player 1 wins this round.';
+    }
+    if (p2Index > p1Index) {
+        updateCount(p2CardCount, p1CardCount);
+        p2CardCount++;
+        p1CardCount--;
+        return 'Player 2 wins this round.';
+
+    }
+    else {
+        return 'It is a tie. Flip again.'
+    }
 }
 
 function flipCard() {
@@ -77,9 +93,25 @@ function flipCard() {
     currentCard++;
 
     //Document round
-    console.log(`Player 1 flips a ${p1Card.value + p1Card.suit}. Player 2 flips a ${p2Card.value + p2Card.suit}. ${rWinner}`)
+    console.log(`
+    Player 1 flips a ${p1Card.value + p1Card.suit}. 
+    Player 2 flips a ${p2Card.value + p2Card.suit}. 
+    ${rWinner}`)
 }
 
+function updateCount(p1CardCount, p2CardCount) {
+    console.log(`
+    Player 1 Cards: ${p1CardCount}
+    Player 2 Cards: ${p2CardCount}
+    `)
+}
+
+// Play game Init
 newDeck();
+
+// while(p1CardCount > 0 && p2CardCount > 0) {
+
 startGame();
 flipCard();
+updateCount();
+// }
